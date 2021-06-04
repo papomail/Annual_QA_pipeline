dbf710df21845a2ff5ab178d56dfcff0a2f23eca
papo <papomail@gmail.com>
2021-06-04 12:12:03 +0100

    made into package

--- scripts/automated_AQA.py
+++ scripts/automated_AQA.py
@@ -11,9 +11,12 @@ import results_to_excel
 
 
 # main_folder = parser.parse_args().main_folder
-main_folder = dicom_organiser.sort_dicoms()
-macro_file,results_dir = test_identifier.main(main_folder) #creates the macro file and outputs its location and the location of the results from FIJI 
-os.system(f'/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx -macro {str(macro_file)}' );
-results_to_excel.main(results_dir)
+def main():
+    main_folder = dicom_organiser.sort_dicoms()
+    macro_file,results_dir = test_identifier.main(main_folder) #creates the macro file and outputs its location and the location of the results from FIJI 
+    os.system(f'/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx -macro {str(macro_file)}' );
+    results_to_excel.main(results_dir)
 
+if __name__ == "__main__":
+    main()
 
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aaqa',
+    version='0.9',
+    description='Automated Annual QA is a set of scripts aimed to eliminate the repetitive manual labour and to improve the reproducibility of the Quality Control data analysis for MRI scanners',
+    author='Patxi Torrealdea ',
+    author_email='francisco.torrealdea@nhs.net',
+    url='https://github.com/papomail/Annual_QA_pipeline',
+    packages=find_packages(),
+
+    install_requires=[
+        "easygui_qt",
+		"pathlib",
+		"pydicom",
+		"pandas",
+		"openpyxl",
+    ],
+    setup_requires=['pytest-runner', 'flake8'],
+    tests_require=['pytest'],
+    package_dir={'mrs':'mrs'},
+    package_data={
+        'scripts.MagNET_QA_scripts':['*'],
+ 
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['aaqa=scripts.automated_AQA:main']
+    }    
+
+)
+
+
