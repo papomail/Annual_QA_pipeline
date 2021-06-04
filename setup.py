4cfc1159b58fee1b6b1fd708336cd9f63e4f958c
papo <papomail@gmail.com>
2021-06-04 13:31:27 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     setup_requires=['pytest-runner', 'flake8'],
     tests_require=['pytest'],
-    # package_dir={'Automated_AQA':'scripts'},
+    package_dir={'scripts'},
     package_data={
         'scripts.MagNET_QA_scripts':['*'],
         'scripts':['*'],
